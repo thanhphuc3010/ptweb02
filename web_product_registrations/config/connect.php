be52a9f40ea97b01a7cd3fb60d6f100eb8d16df0
thanhphuc3010 <ptp60.htttql.neu@gmail.com>
2021-03-17 16:06:17 +0700

    updated table web_product_registrations

---
+++ web_product_registrations/config/connect.php
@@ -0,0 +1,7 @@
+<?php
+    // Database connection
+    $connect = @mysqli_connect("localhost","root","Thanhphuc3010@","ptweb002");  
+    if($connect) {
+    }
+    else echo 'Kết nối thất bại';
+?>
